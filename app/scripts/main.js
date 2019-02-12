1c7696eec9aa91ac3f1f0623aa0ddb3f2187c861
Marco Melluso <foriaa@gmail.com>
2019-02-12 17:39:57 +0800

    first version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('\'Allo \'Allo!');
+
+$(document).ready(function(){
+    var foldblockH;
+
+    $(window).on('scroll',function() {
+        if ($(document).scrollTop() > 100) {
+            foldblockH = $('.foldblock:eq(1)').outerHeight();
+            $(window).off('scroll');
+            console.log(foldblockH);
+            $('.foldblock').removeClass('show');
+            $('.foldblock').height('0px');
+        }
+    });
+
+    $('.seemore').click(function(){
+        var block = $(this).data('block');
+        if($('.foldblock').hasClass('show')){
+            $('.foldblock.show').removeClass('show');
+            $('.foldblock').height('0px');
+        }
+        $('.foldblock.'+block).addClass('show');
+        $('.foldblock.'+block).height(foldblockH);
+    })
+
+    $('#mainmenu a').click(function(event){
+        event.preventDefault();
+        event.stopPropagation();
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+    })
+
+})
